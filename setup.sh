ef826ebb0e39f365b58b135a8a2571777c55cc8b
Nicolas Dupont <npg.dupont@gmail.com>
2014-02-27 17:29:11 +0100

    Install fonts

--- setup.sh
+++ setup.sh
@@ -5,9 +5,19 @@ source="$(pwd)"
 SETUP_DIR=$source/setup
 DOTFILES_DIR=$source/dotfiles
 BASH_DIR=$source/bash
+FONTS_DIR=$source/Fonts
 
-if [ -d "$DOTFILES_DIR" ]; then
+function linkDotfiles() {
 	for file in $(ls $DOTFILES_DIR); do
-		ln -si "$DOTFILES_DIR/$file" $HOME/.$file && echo "Linked $DOTFILES_DIR/$file" 
+		ln -si "$DOTFILES_DIR/$file" $HOME/.$file && echo "Linked $DOTFILES_DIR/$file"
 	done
-fi
+}
+
+function installFonts() {
+	for font in $(ls $FONTS_DIR); do
+		cp -n "$FONTS_DIR/$font" "/Library/Fonts/$font" && echo "$font installed"
+	done
+}
+
+linkDotfiles
+installFonts
